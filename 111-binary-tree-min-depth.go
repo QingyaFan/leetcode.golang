c3b1927abdff3bd89efa9bc70679d718854ef24a
cheerfun <qingyafan@163.com>
2019-05-30 23:47:06 +0800

    sync

---
+++ 111-binary-tree-min-depth.go
@@ -0,0 +1,13 @@
+package main
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func MinDepth(root *TreeNode) int {
+	return 1
+}
