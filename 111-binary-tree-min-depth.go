fe9859a274cf59e57d2fa40f994ea58bc44cc11a
cheerfun <qingyafan@163.com>
2019-06-04 22:41:27 +0800

    sync: 最小深度有点东西 close #3

--- 111-binary-tree-min-depth.go
+++ 111-binary-tree-min-depth.go
@@ -8,6 +8,28 @@ package main
  *     Right *TreeNode
  * }
  */
-func MinDepth(root *TreeNode) int {
-	return 1
+func minDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+
+	if root.Left == nil && root.Right == nil {
+		return 1
+	}
+
+	if root.Left == nil && root.Right != nil {
+		return 1 + minDepth(root.Right)
+	}
+
+	if root.Left != nil && root.Right == nil {
+		return 1 + minDepth(root.Left)
+	}
+
+	leftDep := minDepth(root.Left)
+	rightDep := minDepth(root.Right)
+	if leftDep < rightDep {
+		return 1 + leftDep
+	}
+
+	return 1 + rightDep
 }
