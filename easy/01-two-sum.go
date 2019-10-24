85d8a92e4b64b23ccc7cd15e1658a1685c5e2fa7
cheerfun <qingyafan@163.com>
2019-10-24 23:21:13 +0800

    fix

--- easy/01-two-sum.go
+++ easy/01-two-sum.go
@@ -1,7 +1,7 @@
 package easy
 
-// TwoSum find sum of number is target in nums
-// and return its index in nums
+// TwoSum 给定一个整数数组 nums 和一个目标值 target，
+// 请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
 func TwoSum(nums []int, target int) []int {
 	elMap := make(map[int]int)
 	for i := 0; i < len(nums); i++ {
